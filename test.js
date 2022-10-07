d041e2f0762e5b934d16e50b417122b2c99ba9a1
mishra28soumya <mishra28soumya@gmail.com>
2022-10-07 19:25:06 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
+var XMLHttpRequest = require('xhr2');
 function reqListener () {
   console.log(this.responseText);
 }
 
 const req = new XMLHttpRequest();
 req.addEventListener("load", reqListener);
-req.open("GET", "http://www.googe.com/search?q=http");
+req.open("GET", "https://www.google.com/search?q=http");
 req.send();
